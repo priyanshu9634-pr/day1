37302980ff9a48bf440238784411657f3875d29b
Priyanshu Gupta <varshneyprince326@gmail.com>
2026-01-13 15:14:32 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,21 +1,25 @@
 package com.example.Day1;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class Hello {
 
-    // code yaha aayega
     @GetMapping("/")
-    public  String hello(){
-        return  "This is the first line";
+    public String hello() {
+        return "This is the first Line";
     }
 
-    // Add
+    @GetMapping("/about")
+    public String about() {
+        return "This is the about route";
+    }
 
-
-
+    @GetMapping("/contact")
+    public String contact() {
+        return "This is the contact route";
+    }
 }
+
+
